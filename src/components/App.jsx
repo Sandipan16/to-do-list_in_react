fdddd56e5b0aa97f5901dfc68a6f19f9efa997ee
Sandipan16 <50637809+Sandipan16@users.noreply.github.com>
2020-06-13 21:55:31 +0530

    commit-2
    add characteristics to remove items

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import TodoItems from "./TodoItems";
 
 function App() {
   const [input, setInput] = useState("");
@@ -14,6 +15,14 @@ function App() {
     });
     setInput("");
   }
+
+  function deleteItem(id) {
+    setList(prevValue => {
+      return prevValue.filter((item, index) => {
+        return index !== id;
+      });
+    });
+  }
   return (
     <div className="container">
       <div className="heading">
@@ -27,9 +36,14 @@ function App() {
       </div>
       <div>
         <ul>
-          {list.map(item => {
-            return <li>{item}</li>;
-          })}
+          {list.map((item, index) => (
+            <TodoItems
+              key={index}
+              id={index}
+              text={item}
+              onChecked={deleteItem}
+            />
+          ))}
         </ul>
       </div>
     </div>
